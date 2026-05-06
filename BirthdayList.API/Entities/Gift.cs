ad4f0387fd10da1ee3b3d3f18e5aceae6ba48ddb
OrionC3 <93842749+OrionC3@users.noreply.github.com>
2026-05-06 11:00:55 +0200

    Create gift entitie

---
+++ BirthdayList.API/Entities/Gift.cs
@@ -0,0 +1,13 @@
+﻿namespace BirthdayList.API.Entities
+{
+    public class Gift
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Link { get; set; }
+        public Photo? Photo { get; set; }
+        public bool InStock { get; set; }
+
+    }
+}
